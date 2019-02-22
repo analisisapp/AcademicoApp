9de3d200b1adef661b33407589c7f868db5076d3
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-02-22 14:49:43 -0600

    Sigue sin cargar el masterpage, se creo un archivo bundle.config

--- Academico/Academico/App_Start/BundleConfig.cs
+++ Academico/Academico/App_Start/BundleConfig.cs
@@ -32,7 +32,36 @@ namespace Academico
             // Use la versión de desarrollo de Modernizr para desarrollar y aprender. Luego, cuando esté listo
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                             "~/Scripts/jquery-3.3.1.js",
+                             "~/Scripts/jquery.min.js",
+                             "~/Scripts/bootstrap.min.js",
+                             "~/Scripts/fastclick.js",
+                             "~/Scripts/nprogress.js",
+                             "~/Scripts/Chart.min.js",
+                             "~/Scripts/gauge.min.js",
+                             "~/Scripts/bootstrap-progressbar.min.js",
+                             "~/Scripts/icheck.min.js",
+                             "~/Scripts/skycons.js",
+                             "~/Scripts/date.js",
+                             "~/Scripts/jquery.vmap.min.js",
+                             "~/Scripts/jquery.vmap.world.js",
+                             "~/Scripts/jquery.vmap.sampledata.js",
+                             "~/Scripts/moment.min.js",
+                             "~/Scripts/moment.js",
+                             "~/Scripts/daterangepicker.js",
+                             "~/Scripts/bootstrap-wysiwyg.min.js",
+                             "~/Scripts/jquery.hotkeys.js",
+                             "~/Scripts/prettify.js",
+                             "~/Scripts/jquery.tagsinput.js",
+                             "~/Scripts/switchery.min.js",
+                             "~/Scripts/select2.min.js",
+                             "~/Scripts/parsley.min.js",
+                             "~/Scripts/autosize.min.js",
+                             "~/Scripts/jquery.autocomplete.min.js",
+                             "~/Scripts/starrr.js",
+                             "~/Scripts/custom.js",
                             "~/Scripts/modernizr-*"));
-        }
+
+         }
     }
 }
