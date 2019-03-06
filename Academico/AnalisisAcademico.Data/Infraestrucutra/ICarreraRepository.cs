4ef12ff199bb8de90e36d8d95c5fa8907eb32fb4
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-03-06 14:37:10 -0600

    Se agrego el proyecto de acceso a datos, con la primer tabla catalogo.

---
+++ Academico/AnalisisAcademico.Data/Infraestrucutra/ICarreraRepository.cs
@@ -0,0 +1,15 @@
+﻿using AnalisisAcademico.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisAcademico.Data.Infraestrucutra
+{
+    public interface ICarreraRepository
+    {
+        CatCarrera GetConsultarCatCarrera(int idCarrera);
+        CatCarrera GetConsultarCatCarreraPorUsuario(int idUsuario);
+    }
+}
---
+++ Academico/AnalisisAcademico.Data/Repository/CarreraRepository.cs
@@ -0,0 +1,46 @@
+﻿using AnalisisAcademico.Data.Infraestrucutra;
+using AnalisisAcademico.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisAcademico.Data.Repository
+{
+    public class CarreraRepository: ICarreraRepository
+    {
+        private AnalisisAcademicoEntities contexto = new AnalisisAcademicoEntities();
+
+        public CatCarrera GetConsultarCatCarrera(int idCarrera)
+        {
+            CatCarrera catCarrera = null;
+            try
+            {
+                catCarrera = contexto.CatCarrera.Where(v => v.id == idCarrera).FirstOrDefault<CatCarrera>();
+            }
+            catch (Exception ex)
+            {
+                string errorMensaje = ex.Message;
+              
+            }
+
+            return catCarrera;
+        }
+        public CatCarrera GetConsultarCatCarreraPorUsuario(int idUsuario)
+        {
+            CatCarrera catCarrera = null;
+            try
+            {
+                SegUsuarios usuarios= contexto.SegUsuarios.Where(p => p.id == idUsuario).FirstOrDefault<SegUsuarios>();
+                catCarrera= contexto.CatCarrera.Where(p => p.id == usuarios.idCarrera).FirstOrDefault<CatCarrera>();
+            }
+            catch (Exception ex)
+            {
+
+                string errorMensaje = ex.Message;
+            }
+            return catCarrera;
+        }
+    }
+}
