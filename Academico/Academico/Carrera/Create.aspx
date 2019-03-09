5895a4281d2e0daccf0ff2d4496338a41d7599a1
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-03-08 23:11:01 -0600

    Se agrego la funcionalidad de la inyeccion de dependencias con unity.

---
+++ Academico/Academico/Carrera/Create.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Create.aspx.cs" Inherits="Academico.Carrera.Create" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadPlaceHolder" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ Academico/Academico/Carrera/Create.aspx.cs
@@ -0,0 +1,24 @@
+﻿using AnalisisAcademico.Data.Infraestrucutra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Unity.WebForms.Configuration;
+using Unity;
+using Microsoft.Practices.Unity;
+namespace Academico.Carrera
+{
+    public partial class Create : System.Web.UI.Page
+    {
+        [Dependency]
+        public ICarreraRepository carreraRepository { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
